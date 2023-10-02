d1723f072d2aa26cbad12b9bfab68cbb37b0043d
RafalPelc <rafal.pelc2211@gmail.com>
2023-10-02 18:49:06 +0200

    corrected

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,53 +1,36 @@
-//import { Component } from 'react';
 import { useEffect } from 'react';
-import { createPortal } from 'react-dom';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import css from './Modal.module.css';
 
-const modalRoot = document.querySelector('#modal-root');
-
 export const Modal = ({ largeImageURL, onClose }) => {
+  const handleImageClick = event => {
+    if (event.code === event.arget) {
+      onClose();
+    }
+  };
+
   useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  });
+  }, [onClose]);
 
-  //export class Modal extends Component {
-  //componentDidMount() {
-  //window.addEventListener('keydown', this.handleKeyDown);
-  // }
-
-  // componentWillUnmount() {
-  // window.removeEventListener('keydown', this.handleKeyDown);
-  //  }
-
-  const handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      onClose();
-    }
-  };
-
-  const handleBackDropClick = event => {
-    if (event.code === 'Escape') {
-      //if (event.currentTarget === event.target) {
-      onClose();
-    }
-  };
-
-  //render() {
-  return createPortal(
-    <div className={css.overlay} onClick={handleBackDropClick}>
+  return (
+    <div className={css.overlay} onClick={handleImageClick}>
       <div className={css.modal}>
-        <img src={largeImageURL} alt="" width={1100} height={800} />
+        <img src={largeImageURL} alt="" width={1000} height={700} />
       </div>
-    </div>,
-    modalRoot
+    </div>
   );
 };
 
 Modal.propTypes = {
-  onClose: propTypes.func,
-  largeImageURL: propTypes.string.isRequired,
+  onClose: PropTypes.func,
+  largeImageURL: PropTypes.string.isRequired,
 };
